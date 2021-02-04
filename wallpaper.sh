418ba349fef71587ba9a49df35775dee5ce462a6
Siddharth Saini <53482124+siddharthsaini@users.noreply.github.com>
2021-02-05 00:16:36 +0530

    ./wallpaper.sh in terminal to execute

---
+++ wallpaper.sh
@@ -0,0 +1,24 @@
+﻿
+#!/bin/sh
+check_status()
+{
+    path
+    file
+}
+path()
+{
+#full path to you wallpaper folder example shown below:
+find "/home/sid/Images/Wallpapers"
+}
+file()
+{
+fullpath=$(path)
+prefix="file:///"
+gsettings_path="${prefix}${fullpath}"
+gsettings set org.gnome.desktop.background picture-uri $gsettings_path
+}
+while :
+do
+    check_status
+    sleep 0.5 # 1value=1second. Eg: 300 = 5 mins
+done
